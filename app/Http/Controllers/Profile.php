416b91321f8318a0c8a201f1acc4e1bb1594c026
Christiella Abinosy <christiellasetiawan24@gmail.com>
2023-06-20 13:26:42 +0700

    update Profile Backend
    butuh review

--- app/Http/Controllers/Profile.php
+++ app/Http/Controllers/Profile.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class Profile extends Controller
+class ProfileController extends Controller
 {
-    
+    /**
+     * Get user profile.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show()
+    {
+        $user = Auth::user();
+        return response()->json($user);
+    }
+
+    /**
+     * Update user profile.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([ 
+            'username' => 'required|string',
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|password',
+        ]);
+
+        $user->update($validatedData);
+
+        return response()->json($user);
+    }
 }
