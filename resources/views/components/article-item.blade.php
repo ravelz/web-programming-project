7ae4e1af2fd4feaf6670663563867638cc739b14
renathatuwaidan <rtuwaidan14@gmail.com>
2023-07-10 22:42:31 +0700

    update : buttons read article

--- resources/views/components/article-item.blade.php
+++ resources/views/components/article-item.blade.php
@@ -12,23 +12,23 @@
             <div class="card-body" style="height: auto">
                 <div class="d-flex ">
                     <div class="me-auto">
-                        <h5 id = "follow-cat" class="card-title p-2" style="color:#982727;font-size:20px; font-weight:600; font-family:'Rubik', sans-serif;
+                        <h5 id = "follow-cat" class="card-title p-2" style="color:#982727;font-size:20px; font-weight:600; 
                         padding-left:32px; padding-top:16px; box-sizing:border-box">Budaya</h5>
                     </div>
                     <div>
-                        <h5 id = "follow-time"class="card-title " style="color:#982727;font-size:20px; font-weight:400; font-family:'Rubik', sans-serif;
+                        <h5 id = "follow-time"class="card-title " style="color:#982727;font-size:20px; font-weight:400; 
                         padding-top:8px; padding-right:32px;box-sizing:border-box">{{ $article->differenceDate }} hari yang lalu</h5>
                     </div>
                 </div>
             
-                <div id = "follow-tit" style="max-height:400px;"><p class="card-text" style="padding-left: 8px;color:black;font-size:34px; font-weight:400; font-family:'Rubik', sans-serif;">{{ $article->judul }}</p></div>
+                <div id = "follow-tit" style="max-height:400px;"><p class="card-text" style="padding-left: 8px;color:black;font-size:34px; font-weight:400; ">{{ $article->judul }}</p></div>
                 <div id = "follow-desc"style="max-height:400px">
                 <p class="card-text" style="padding-left: 8px;color:background: #374A59;
-                    ;font-size:20px; font-weight:400; font-family:'Rubik', sans-serif; padding-top:24px">{{ $article->deskripsi }}</p>
+                    ;font-size:20px; font-weight:400;  padding-top:24px">{{ $article->deskripsi }}</p>
                 </div>                
                 <div class="d-flex flex-row p-2">
                     <img style = "margin-bottom: 8px;"id = "follow-img"src="{{ asset('storage') }}/upload/Profile.jpg" class="img-fluid rounded-circle " alt="..." width="44px" height="44px">
-                    <a href = "{{ route('profile', ['username' => $article->username]) }}" id = "follow-name" style="text-decoration:none;color:#5E5D2D;font-size:20px; font-weight:600; font-family:'Rubik', sans-serif;">{{ $article->authorName }}</a>
+                    <a href = "{{ route('profile', ['username' => $article->username]) }}" id = "follow-name" style="text-decoration:none;color:#5E5D2D;font-size:20px; font-weight:600; ">{{ $article->authorName }}</a>
                     
                     <div class="ms-auto" style="margin-top:8px">
                         <a href = "{{route('read', ['id'=>$article->id_article, 'judul'=>$article->judul])}}"style="text-decoration : none; padding-top: 8px; padding-right:32px;color:#5E5D2D;font-size:20px; font-weight:600; font-family:'Rubik', sans-serif;">Baca Lebih 
--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -6,13 +6,13 @@
                     <div class="d-flex flex-row align-items-center">
                         <img class="img-fluid" src="{{ asset('storage/'.$comment->profile_picture) }}" style= "width: 50px;height: 50px;border-radius: 100%;float:left;" alt="">
                         <div class="d-flex flex-column px-3 justify-content-center">
-                            <h4 style="font-family: Poppins, sans-serif;font-weight: 600;"> {{ $comment->name }} </h4>
+                            <h4 style="font-weight: 600;"> {{ $comment->name }} </h4>
                             <p> {{ $comment->created_at }} </p>
                         </div>
                     </div>
                 </div>
                 <div class="row" style="padding-top: 10px; margin-bottom: 5px">
-                    <h6 style="font-family: Poppins, sans-serif; padding-top: 10px"> {{$comment->comment}} </h6>
+                    <h6 style="padding-top: 10px"> {{$comment->comment}} </h6>
                 </div>
             </div>
         </div>
